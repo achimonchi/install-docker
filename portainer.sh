a55f285f256b1f37fcc5a7420d4cc27edef99879
Reyhan Jovie <achimonchi.reyhan@gmail.com>
2020-11-11 07:50:48 +0000

    add portainer container script

---
+++ portainer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+command=$1
+
+if [ $command = "RUN"  ]
+then
+	docker volume create portainer_data
+
+	docker run -d \
+	-p 8000:8000 -p 9000:9000 \
+	--name=portainer \
+	--restart=always \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v portainer_data:/data \
+	portainer/portainer-ce
+elif [ $command = "STOP"  ]
+then
+	docker rm -f portainer
+	docker volume rm portainer_data
+
+fi
